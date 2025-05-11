3d637b80f4ac4332cf396cab9f3f925209fe0f4f
angelshinh1 <angelshinh2023@gmail.com>
2025-05-11 13:26:57 -0400

    Initialized repository structure

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const PORT = process.env.PORT || 5000;
+
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Yo, what's good?");
+});
+
+app.use((req, res) => {
+    res.status(`404`).send("Not Found");
+});
+
+
+app.listen(PORT, () => {
+    console.log("Server is running");
+});
